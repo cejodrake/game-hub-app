799a1700b06b9b3d92c2c45cd626da9dff8a72ba
Juan C Calix <j_calix2002@hotmail.com>
2023-04-28 09:02:44 -0700

    ship gnres with the app

--- src/hooks/useGame.ts
+++ src/hooks/useGame.ts
@@ -14,6 +14,7 @@ export interface Game {
   backgroun_image: string;
   parent_platforms: { platform: Platform }[];
   metacritic: number;
+  rating_top: number;
 }
 
 const useGames = (gameQuery: GameQuery) =>
--- src/hooks/useGenre.ts
+++ src/hooks/useGenre.ts
@@ -1,4 +1,4 @@
-import useData from "./useData";
+import Genres from "../data/genres";
 
 export interface Genre {
   id: number;
@@ -6,6 +6,6 @@ export interface Genre {
   image_background: string;
 }
 
-const useGenre = () => useData<Genre>("/genres");
+const useGenre = () => ({ data: Genres, isLoading: false, error: null });
 
 export default useGenre;
